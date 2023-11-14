c994a7d40861f4b988047090cd10fd2a3b16a9d3
limbocingo <mrcingosup@gmail.com>
2023-11-14 19:11:54 +0100

    solve CORS problem

--- backend/utils/authentication.py
+++ backend/utils/authentication.py
@@ -13,16 +13,17 @@ from backend.models import *
 def authenticate(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
+        response = func(*args, **kwargs)
+
         try:
             user = User.get(User.sid == flask.request.headers.get('Authorization'))
+
+            if user.administrator == False:
+                response = flask.jsonify({'message': 'You are not authorized.'}), 401
         except peewee.DoesNotExist:
-            return flask.jsonify({'message': 'You are not authorized.'}), 401
+            response = flask.jsonify({'message': 'You are not authorized.'}), 401
+
+        response[0].headers['Access-Control-Allow-Origin'] = '*'
         
-        if user.administrator == False:
-            return flask.jsonify({'message': 'You are not authorized.'}), 401
-
-        response =  func(*args, **kwargs)
-        response.headers['Access-Control-Allow-Origin'] = '*'
-
         return response
     return wrapper
